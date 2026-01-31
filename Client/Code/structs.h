82fcd080073416534b70f7c34cb1bd391cc63771
Cole Puckett <cjp7e@mtmail.mtsu.edu>
2026-01-31 16:52:42 -0600

    Added clients, action, line, and gameState structs to structs.h

--- Client/Code/structs.h
+++ Client/Code/structs.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <chrono>
 
 struct Point {
 	int x;
@@ -7,4 +8,37 @@ struct Point {
 		x = x1;
 		y = y1;
 	}
+};
+
+struct Line {
+	Point a;
+	Point b;
+};
+
+struct Action {
+	std::string clientId;
+	enum tool {
+		eraser,
+		pencil,
+		bucket
+	};
+	int brushSize;
+	Line points;
+	std::string color;
+
+};
+
+struct Clients {
+	std::string clientId;
+	std::string name;
+	int score;
+	bool connected;
+	std::chrono::system_clock::time_point lastTime;
+};
+
+struct GameState {
+	std::string currentWord;
+	int round;
+	std::string drawingId;
+	Action draw;
 };
