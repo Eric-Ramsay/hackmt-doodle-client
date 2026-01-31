64bf77c6320d6bdc7220711842c783998c99c261
Cole Puckett <cjp7e@mtmail.mtsu.edu>
2026-01-31 01:39:33 -0600

    redownloaded httplib library without renaming. Works but requires having openssl downloaded

--- Client/Code/main.cpp
+++ Client/Code/main.cpp
@@ -1,8 +1,17 @@
+#define CPPHTTPLIB_OPENSSL_SUPPORT
 #include <iostream>
-
+#include "httplib.h"
 
 int main() {
-	std::cout << "Hello World Test" << std::endl;
-	
-	return 0;
+    std::cout << "Hello World Test\n";
+
+    httplib::SSLClient cli("dummy.restapiexample.com", 443);
+
+    auto res = cli.Get("/api/v1/employees");
+
+
+    std::cout << "Status: " << res->status << "\n";
+    std::cout << "Body:\n" << res->body << "\n";
+
+    return 0;
 }
