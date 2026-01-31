018f81991b242e5bdd9bb25e51a4a10d3285af0e
Donald Thirakul <donald_t@comcast.net>
2026-01-31 00:58:14 -0600

    First http get request attempt

--- Client/Code/main.cpp
+++ Client/Code/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
-
+#include "libhttp.h"
 
 int main() {
 	std::cout << "Hello World Test" << std::endl;
+	// HTTPS
+	//httplib::Client cli("https://dummy.restapiexample.com");
+	httplib::Client cli("https://google.com");
+
+
+	if (httplib::Result res = cli.Get("/")) {
+		int statusCode = res->status;
+		//res->body;
+		std::cout << statusCode << std::endl;
+
+	}
 	
 	return 0;
 }
